4cc1fa30393068363d3f1ded2ca84dbd049cc8c4
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-04-29 10:29:52 -0500

    first commit to homework 6

---
+++ src/test/java/com/zerobank/runners/CukesRunner.java
@@ -0,0 +1,20 @@
+package com.zerobank.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue ="com\\zerobank\\stepdefinitions",
+        features = "src\\test\\resources\\features",
+        dryRun = false,
+        strict = true,
+        tags  ="",
+        plugin = {"html:target\\default-report",
+                "json:target\\cucumber.json"}
+
+)
+public class CukesRunner {
+}
